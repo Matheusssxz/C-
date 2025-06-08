02bb4a39b4107ab07127d2a8b70dda14dadb6e7a
= <=nicolascajueiro098@gmail.com>
2025-06-08 20:35:50 -0300

    algoritmos se ordenação

--- main.cpp
+++ main.cpp
@@ -27,75 +27,40 @@ void inicializar(TLista *lista)
   lista->fim = NULL;
   lista->tam = 0;
 }
-void removerTarefa(TLista *lista, Ttarefa *tarefa)
-{
-  if (lista->inicio == NULL)
-  {
-    return;
-  }
-
-  No *iterador = lista->inicio;
-  No *noARemover = NULL;
-
-  if (iterador->tarefa.nome == tarefa->nome &&
-      iterador->tarefa.prioridade == tarefa->prioridade &&
-      iterador->tarefa.tempo == tarefa->tempo)
-  {
-    noARemover = iterador;
-    lista->inicio = iterador->proximo;
-
-    if (lista->inicio == NULL)
-    {
-      lista->fim = NULL;
-    }
-
-    free(noARemover);
-    lista->tam--;
-    return;
-  }
-
-  while (iterador->proximo != NULL &&
-         (iterador->proximo->tarefa.nome != tarefa->nome ||
-          iterador->proximo->tarefa.prioridade != tarefa->prioridade ||
-          iterador->proximo->tarefa.tempo != tarefa->tempo))
-  {
-    iterador = iterador->proximo;
-  }
-
-  if (iterador->proximo != NULL)
-  {
-    noARemover = iterador->proximo;
-    iterador->proximo = noARemover->proximo;
-
-    if (iterador->proximo == NULL)
-    {
-      lista->fim = iterador;
-    }
-
-    free(noARemover);
-    lista->tam--;
-  }
-}
 
 void removerTarefa(TLista *lista)
 {
   if (lista->inicio == NULL)
   {
-    std::cout << "pilha vazia\n";
+    std::cout << "Nenhuma tarefa para ser removida\n";
     return;
   }
 
-  No *remover = lista->inicio;
-  lista->inicio = remover->proximo;
+  No *remover;
 
-  if (lista->inicio == NULL)
+  if (lista->inicio == lista->fim)
   {
+    remover = lista->inicio;
+    lista->inicio = NULL;
     lista->fim = NULL;
+  }
+  else
+  {
+
+    No *atual = lista->inicio;
+    while (atual->proximo != lista->fim)
+    {
+      atual = atual->proximo;
+    }
+
+    remover = lista->fim;
+    atual->proximo = NULL;
+    lista->fim = atual;
   }
 
   free(remover);
   lista->tam--;
- cout << "Tarefa removida com sucesso";
+  std::cout << "Tarefa removida com sucesso\n";
 }
 
 void InserirTarefa(TLista *lista, Ttarefa *tarefa)
@@ -104,19 +69,72 @@ void InserirTarefa(TLista *lista, Ttarefa *tarefa)
   strcpy(novo->tarefa.nome, tarefa->nome);
   novo->tarefa.tempo = tarefa->tempo;
   novo->tarefa.prioridade = tarefa->prioridade;
+  novo->proximo = NULL;
 
   if (lista->inicio == NULL)
   {
-    novo->proximo = NULL;
     lista->inicio = novo;
     lista->fim = novo;
   }
   else
   {
-    novo->proximo = lista->inicio;
-    lista->inicio = novo;
+    lista->fim->proximo = novo;
+    lista->fim = novo;
   }
   lista->tam++;
+}
+void exibirTarefaFila(TLista *lista)
+{
+  No *iterador = lista->inicio;
+  if (lista->inicio == NULL)
+  {
+    std::cout << "Nenhuma tarefa a ser executada.\n";
+    return;
+  }
+  else
+  {
+    while (iterador != NULL)
+    {
+      std::cout << "Nome: " << iterador->tarefa.nome << "\n";
+      iterador = iterador->proximo;
+    }
+  }
+}
+
+void bubbleSortLista(TLista *lista)
+{
+  if (lista->inicio == NULL || lista->inicio->proximo == NULL)
+    return;
+
+  bool trocou;
+  TNo *ptr1;
+  TNo *lptr = NULL;
+
+  do
+  {
+    trocou = false;
+    ptr1 = lista->inicio;
+
+    while (ptr1->proximo != lptr)
+    {
+      if (ptr1->tarefa.prioridade > ptr1->proximo->tarefa.prioridade)
+      {
+        Ttarefa temp = ptr1->tarefa;
+        ptr1->tarefa = ptr1->proximo->tarefa;
+        ptr1->proximo->tarefa = temp;
+
+        trocou = true;
+      }
+      ptr1 = ptr1->proximo;
+    }
+    lptr = ptr1;
+  } while (trocou);
+}
+void insertionSort(TLista *lista)
+{
+  if (lista->inicio == NULL || lista->inicio->proximo == NULL)
+    return;
+    
 }
 
 void mostrarMenu()
@@ -129,7 +147,7 @@ void mostrarMenu()
 
   do
   {
-    std::cout << "Escolha as opcoes\n";
+    std::cout << "\nEscolha as opcoes\n";
     std::cout << "1- Adicionar tarefas\n";
     std::cout << "2- Remover tarefas\n";
     std::cout << "3- Listar tarefas\n";
@@ -141,16 +159,23 @@ void mostrarMenu()
     case 1:
       std::cout << "Adicione uma tarefa:\n";
       std::cout << "Tarefa:\n";
-      std::cin.getline(tarefa.nome,100);
+      std::cin.getline(tarefa.nome, 100);
 
       std::cout << "Tempo (min):\n";
       std::cin >> tarefa.tempo;
       std::cin.ignore();
 
-      std::cout << "Prioridade:\n";
+      std::cout << "Prioridade(1 a 5 || enquanto menor o valor maior prioridade || enquanto maior o valor menor prioridade):\n";
       std::cin >> tarefa.prioridade;
       std::cin.ignore();
-      InserirTarefa(&lista, &tarefa);
+      if (tarefa.prioridade > 5)
+      {
+        cout << "Prioridades vao ate 5, favor, adicionar uma prioridade valida";
+      }
+      else
+      {
+        InserirTarefa(&lista, &tarefa);
+      }
       break;
     case 2:
       removerTarefa(&lista);
@@ -163,16 +188,24 @@ void mostrarMenu()
       }
       else
       {
-        struct No *atual = lista.inicio;
-        while (atual != NULL)
+        std::cout << "1 - Exibir por chegada\n";
+        std::cout << "2 - Exibir por prioridade\n";
+        std::cout << "3 - Exibir por tempo\n";
+        std::cout << "4 - Exibir por nome\n";
+        std::cin >> input;
+        std::cin.ignore();
+        if (input == 1)
         {
-          std::cout << "Tarefa: " << atual->tarefa.nome
-                    << " | Tempo: " << atual->tarefa.tempo
-                    << " | Prioridade: " << atual->tarefa.prioridade
-                    << "\n";
-          atual = atual->proximo;
+          exibirTarefaFila(&lista);
+        }
+        else if (input == 2)
+        {
+
+          bubbleSortLista(&lista);
+          exibirTarefaFila(&lista);
         }
       }
+
       break;
     case 4:
       std::cout << "Saindo...";
